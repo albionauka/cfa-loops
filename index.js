bb15730970fd0b471090c0c31be633769b0d84f5
albionauka <albionauka@gmail.com>
2021-10-12 18:38:10 -0700

    Finished the loop Activity

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 // LOOPS
 
 // 1. create a loop that prints out the numbers 0 - 5 using a while loop
+let count = 0;
+while (count < 6){
+    console.log(count);
+    count += 1;
+}
 
 // 2. create a loop that prints out the numbers 0 - 5 using a for loop
-
+for (let i= 0; i <= 6; i+= 1) {
+    //console.log(i);
+}
 // 3. create a loop that prints out the numbers 0 - 5 using a for loop but for each number return the squared version
-
+for (let f = 0; f <= 5; f += 1){
+    console.log(f * f);
+}
 //4. iterate over the string below and print out each character
 let string = "the fox jumps over the moon"; 
+let fox = "hello";
+for (let o = 0; o < fox.length; o +=1) {
+    console.log(fox[o]);
+}
 
